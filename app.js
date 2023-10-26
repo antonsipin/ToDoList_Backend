96de0e42f465140bc13d7e38c3cd3e914115b895
Anton Sipin <anton.sipin@gmail.com>
2023-10-26 14:14:29 +0300

    Fix: static

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static(path.resolve('../frontend/build')))
+app.use(express.static(path.resolve('../client/build')))
 
 app.use(
   session({
--- src/controllers/main-controller.js
+++ src/controllers/main-controller.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-const path = require('path');
+require('dotenv').config()
+const path = require('path')
 
 const index = async (req, res) => {
 
